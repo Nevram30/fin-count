e936be49704c88a23f7fe489b8f93d92f22abe80
Nevram30 <68675200+Nevram30@users.noreply.github.com>
2025-11-16 03:00:43 +0800

    feat: add new logo loading state

--- src/app/components/ui/fullscreen.loader.tsx
+++ src/app/components/ui/fullscreen.loader.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const FullScreenLoader: React.FC<React.PropsWithChildren> = ({ children }) => {
     return (
         <div className="w-screen h-screen grid content-center bg-white">
             <div className="flex gap-3 items-center justify-center h-[300px]">
-                <div className="w-12 h-12 border-4 border-blue-200 rounded-full animate-spin border-t-blue-500">
-                    <p className="text-slate-700">{children}</p>
+                <div className="relative w-24 h-24 flex items-center justify-center">
+                    <div className="absolute w-24 h-24 border-4 border-blue-200 rounded-full animate-spin border-t-blue-500"></div>
+                    <Image
+                        src="/image/logo.png"
+                        alt="FinCount Logo"
+                        width={60}
+                        height={60}
+                        className="object-contain z-10"
+                    />
                 </div>
+                <p className="text-slate-700">{children}</p>
             </div>
         </div>
     )
